47eea369c700af5b7f85c6842e6e8b6c6353381c
Mhmd Sleimane <mohammad.sleimane@isae.edu.lb>
2023-08-17 11:02:08 +0300

    Challenge 2 - Done with step 1

---
+++ JavaScript Challenges/Challenge 2/script.js
@@ -0,0 +1,11 @@
+let age=18;
+if(age>=20)console.log("you can enter this room");
+else console.log("you can't enter this room");
+const expression1 = 5 > 3;
+const expression2 = 10 === '10';
+const expression3 = true && false;
+const expression4 = !expression3;
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
