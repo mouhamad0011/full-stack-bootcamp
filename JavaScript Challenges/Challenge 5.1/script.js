0a309b1d1f12383e9b76b2a8f79d675a147c8e55
Mhmd Sleimane <mohammad.sleimane@isae.edu.lb>
2023-08-24 15:46:27 +0300

    Challenge 5.1 - Done with step 2

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -11,3 +11,20 @@ button.addEventListener("click",function(){
     button.style.color="white";
     button.textContent="Clicked "+Math.random()*1000+1;
 })
+const heading1=document.createElement("h1");
+heading1.style.border="10px dotted green";
+heading1.textContent="hello";
+document.body.appendChild(heading1);
+document.addEventListener('keydown', function(event) {
+    switch (event.key) {
+      case 'a':heading1.style.backgroundColor = 'red'; break;
+      case 's': heading1.style.marginLeft = parseInt(getComputedStyle(heading1).marginLeft) + 10 + 'px'; break;
+      case 'd': const newParagraph = document.createElement('p');
+                newParagraph.textContent = 'Key D was pressed!';
+                document.body.appendChild(newParagraph);break;
+      case 'w':heading1.style.display = heading1.style.display === 'none' ? 'block' : 'none';break;
+      case ' ':const currentFontSize = parseInt(getComputedStyle(heading1).fontSize);
+               heading1.style.fontSize = (currentFontSize + 2) + 'px';break;
+    }
+  });
+  
