9fa22483b4444385872ec727b2967c393bd65a6a
Mhmd Sleimane <mohammad.sleimane@isae.edu.lb>
2023-08-24 20:06:08 +0300

    done

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -27,4 +27,61 @@ document.addEventListener('keydown', function(event) {
                heading1.style.fontSize = (currentFontSize + 2) + 'px';break;
     }
   });
-  
+  const form = document.createElement('form');
+  const fullNameInput = document.createElement('input');
+  const emailInput = document.createElement('input');
+  const passwordInput = document.createElement('input');
+  const confirmPasswordInput = document.createElement('input');
+  const but=document.createElement("button");
+  const errorMessage = document.createElement('p');
+  const successMessage = document.createElement('p');
+  document.body.appendChild(form);
+  form.appendChild(fullNameInput);
+  form.appendChild(emailInput);
+  form.appendChild(passwordInput);
+  form.appendChild(confirmPasswordInput);
+  form.appendChild(but);
+  document.body.appendChild(errorMessage);
+  document.body.appendChild(successMessage);
+  but.textContent="submit";
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    errorMessage.textContent = '';
+    successMessage.textContent = '';
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(emailInput.value)) {
+      errorMessage.textContent = 'Please enter a valid email address.';
+    } else if (passwordInput.value !== confirmPasswordInput.value) {
+      errorMessage.textContent = 'Passwords do not match.';
+    } else {
+      successMessage.textContent = 'Form submitted successfully!';
+    }
+  });
+
+  document.addEventListener('click', function(event) {
+    if (!event.target.matches('input')) {
+      fullNameInput.style.backgroundColor = '';
+      emailInput.style.backgroundColor = '';
+      passwordInput.style.backgroundColor = '';
+      confirmPasswordInput.style.backgroundColor = '';
+    }
+  });
+
+  fullNameInput.addEventListener('focus', () => {
+    fullNameInput.style.backgroundColor = '#f0f0f0';
+  });
+
+  emailInput.addEventListener('focus', () => {
+    emailInput.style.backgroundColor = '#f0f0f0';
+  });
+
+  passwordInput.addEventListener('focus', () => {
+    passwordInput.style.backgroundColor = '#f0f0f0';
+  });
+
+  confirmPasswordInput.addEventListener('focus', () => {
+    confirmPasswordInput.style.backgroundColor = '#f0f0f0';
+  });
