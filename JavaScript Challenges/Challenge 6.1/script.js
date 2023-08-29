8b24732bd51f2b90f30943a1554a58202301f1df
Mhmd Sleimane <mohammad.sleimane@isae.edu.lb>
2023-08-29 09:45:45 +0300

    Challenge 6.1 - Done with Step 3

--- JavaScript Challenges/Challenge 6.1/script.js
+++ JavaScript Challenges/Challenge 6.1/script.js
@@ -9,7 +9,7 @@ delay(2000).then(() => {
     console.log("Delayed action after 2000 milliseconds");
   });
 
-  
+
   function fetchRandomFact() {
     return fetch("https://uselessfacts.jsph.pl/random.json?language=en")
       .then(response => response.json())
@@ -23,4 +23,22 @@ delay(2000).then(() => {
     .catch(error => {
       console.error("An error occurred:", error);
     });
-  
+    async function fetchRandomFact() {
+        try {
+          const response = await fetch("https://uselessfacts.jsph.pl/random.json?language=en");
+          const data = await response.json();
+          return data.text;
+        } catch (error) {
+          throw new Error("An error occurred: " + error);
+        }
+      }
+      
+      (async () => {
+        try {
+          const fact = await fetchRandomFact();
+          console.log("Random Fact:", fact);
+        } catch (error) {
+          console.error(error.message);
+        }
+      })();
+      
