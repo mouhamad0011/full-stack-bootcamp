4c421db2278021cc3a2ce49984aca89df4211cdb
Mhmd Sleimane <mohammad.sleimane@isae.edu.lb>
2023-08-29 09:41:17 +0300

    Challenge 6.1 - Done with Step 2

--- JavaScript Challenges/Challenge 6.1/script.js
+++ JavaScript Challenges/Challenge 6.1/script.js
@@ -8,4 +8,19 @@ function delay(milliseconds) {
 delay(2000).then(() => {
     console.log("Delayed action after 2000 milliseconds");
   });
-    
+
+  
+  function fetchRandomFact() {
+    return fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+      .then(response => response.json())
+      .then(data => data.text);
+  }
+  
+  fetchRandomFact()
+    .then(fact => {
+      console.log("Random Fact:", fact);
+    })
+    .catch(error => {
+      console.error("An error occurred:", error);
+    });
+  
